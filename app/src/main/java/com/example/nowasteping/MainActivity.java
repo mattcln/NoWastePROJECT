c8b72f280e047d11b9afbe3af4d4b7548830fcf9
mattisterik <mattisterik@users.noreply.github.com>
2020-03-31 20:36:07 +0200

    Accueil 1.1
    Vérif si la combi mail & mdp est bonne

--- app/src/main/java/com/example/nowasteping/MainActivity.java
+++ app/src/main/java/com/example/nowasteping/MainActivity.java
@@ -13,37 +13,55 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView lbltitre;
     private TextView lblMail;
     private TextView lblMdp;
     private EditText txtMail;
     private EditText txtMdp;
     private Button btnConnect;
     private Button btnInscription;
-    private ImageView imgZucc;
+    private ImageView imgNoWaste;
+    private TextView txtWrongmdp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lbltitre = findViewById(R.id.lblTitre);
         lblMail = findViewById(R.id.lblMail);
         lblMdp = findViewById(R.id.lblMdp);
         txtMail = findViewById(R.id.txtMail);
         txtMdp = findViewById(R.id.txtMdp);
         btnConnect = findViewById(R.id.btnConnect);
         btnInscription = findViewById(R.id.btnInscription);
-        imgZucc = findViewById(R.id.imgZucc);
+        imgNoWaste = findViewById(R.id.imgNoWaste);
+        txtWrongmdp = findViewById(R.id.txtWrongmdp);
+        txtWrongmdp.setVisibility(View.INVISIBLE);
 
         btnConnect.setOnClickListener(btnSeConnecterListener);
+        init();
+    }
 
+    private void init() {
+        txtMail.setText("");
+        txtMdp.setText("");
+        txtMail.requestFocus();
     }
 
     private View.OnClickListener btnSeConnecterListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Log.i("DEBUG", "Bouton 'Se connecter' cliqué");
+
+            String textMail = txtMail.getText().toString();
+            String textMdp = txtMdp.getText().toString();
+            Log.i("DEBUG",textMail + " " + textMdp);
+
+            if(textMail.equals("nowaste@gmail.com") && textMdp.equals("1234")){
+                Log.i("DEBUG","Authentification successfull");
+            } else {
+                Log.i("DEBUG","Authentification failed");
+                txtWrongmdp.setVisibility(View.VISIBLE);
+            }
         }
     };
 }
